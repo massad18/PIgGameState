1cb63038881d98e3ce43f4c6bed53caae4c12661
KeokiMassad <massad18@up.edu>
2016-10-12 15:39:59 -0700

    RollAction CP 2

--- app/src/main/java/edu/up/cs301/pig/PigRollAction.java
+++ app/src/main/java/edu/up/cs301/pig/PigRollAction.java
@@ -1,7 +1,14 @@
 package edu.up.cs301.pig;
 
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
 /**
  * Created by vandewar19 on 10/12/2016.
  */
-public class PigRollAction {
+public class PigRollAction extends GameAction {
+
+    public PigRollAction (GamePlayer player) {
+        super(player);
+    }
 }
